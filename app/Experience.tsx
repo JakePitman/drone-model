0faf2dc8e97bcc0723570c500cc172520f470f12
Jake Pitman <jakeseanp@gmail.com>
2023-09-26 00:21:47 +1300

    Add drone component

--- app/Experience.tsx
+++ app/Experience.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
+import { OrbitControls, Environment } from "@react-three/drei";
+import { Drone } from "./Drone.tsx";
 
 export const Experience = () => {
   return (
@@ -10,10 +11,9 @@ export const Experience = () => {
       <OrbitControls />
       <ambientLight />
 
-      <mesh rotation={[Math.PI * 0.25, Math.PI * 0.25, 0]}>
-        <boxGeometry args={[1, 1, 1]} />
-        <meshNormalMaterial />
-      </mesh>
+      <Environment preset="city" background />
+
+      <Drone />
     </Canvas>
   );
 };
